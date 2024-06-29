e9c05a08d7d6323d861356b97fc694a6f567fe4a
aartiwadagaonkar <106589221+aartiwadagaonkar@users.noreply.github.com>
2024-06-29 14:54:16 +0530

    user controller update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@
 const asyncHandler= require("express-async-handler")
 const Order=require("../models/Order")
 exports.userGetAllOrders= asyncHandler( async (req,res)=>{
-    const result=await Order.find({user:req.params.id}).sort({createdAt:1}).populate("products.product")
+    const result=await Order.find({user:req.params.id}).sort({createdAt:-1}).populate("products.product")
 
     res.json({message:" User fetch order  Success",result})
 })
