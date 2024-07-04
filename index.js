503285a8e0e4525f1eca2150edb932c7db91edaa
aartiwadagaonkar <106589221+aartiwadagaonkar@users.noreply.github.com>
2024-07-04 13:51:17 +0530

    change in index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express=require("express")
 const cors=require("cors")
 require("dotenv").config()
 const app = express()
+app.use(express.static("public"))
 app.use(cors({
     origin:true,
     credentials:true
